be17de22949ad06e614b20706d75ec07c0255d06
yesi3 <73842524+yesi3@users.noreply.github.com>
2023-03-07 13:58:45 +0100

    Add files via upload

---
+++ Deluxe Properties/js/index.js
@@ -0,0 +1,30 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+    0: {
+      slidesPerView: 1,
+    },
+
+    520: {
+      slidesPerView: 2,
+    },
+
+    950: {
+      slidesPerView: 3,
+    },
+  }
+});
+
